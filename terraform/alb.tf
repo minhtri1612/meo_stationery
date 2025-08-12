6870b498b8a60acc32ffe76bdacdd28fbfbc60ef
Minh Tri <10422119@student.vgu.edu.vn>
2025-08-12 09:38:43 +0700

    done

---
+++ terraform/alb.tf
@@ -0,0 +1,72 @@
+# Application Load Balancer
+resource "aws_lb" "main" {
+  name               = "food-app-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = [aws_subnet.public.id, aws_subnet.public2.id]
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "food-app-alb"
+  }
+}
+
+# ALB Security Group
+resource "aws_security_group" "alb" {
+  name        = "alb-security-group"
+  description = "ALB Security Group"
+  vpc_id      = aws_vpc.custom.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
+  }
+}
+
+# ALB Target Group
+resource "aws_lb_target_group" "app" {
+  name        = "food-app-tg"
+  port        = 3000
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.custom.id
+  target_type = "ip"
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    interval            = 30
+    matcher             = "200"
+    path                = "/"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    timeout             = 5
+    unhealthy_threshold = 2
+  }
+}
+
+# ALB Listener
+resource "aws_lb_listener" "app" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,8 @@ output "ec2_public_dns" {
   description = "Public DNS name of the EC2 instance"
   value       = aws_instance.web.public_dns
 }
+
+output "ecr_repository_url" {
+  description = "The URL of the created ECR repository"
+  value       = aws_ecr_repository.my_app.repository_url
+}
